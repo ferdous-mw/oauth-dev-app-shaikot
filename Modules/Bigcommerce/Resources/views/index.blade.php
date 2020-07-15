b3aa991b0959d589af847e4e72b430091a36d294
ferdous-mw <ferdous@medisuware.com>
2020-07-15 10:48:15 +0600

    module:bigcommerce:create

---
+++ Modules/Bigcommerce/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('bigcommerce::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('bigcommerce.name') !!}
+    </p>
+@endsection
